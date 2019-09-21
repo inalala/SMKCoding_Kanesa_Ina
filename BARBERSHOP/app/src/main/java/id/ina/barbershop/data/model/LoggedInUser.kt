bc614afd7ea68c8fa091ef69eae2d1f81be9e35e
inanur03 <inalala05@gmail.com>
2019-09-21 10:13:55 +0700

    perubahan

---
+++ BARBERSHOP/app/src/main/java/id/ina/barbershop/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package id.ina.barbershop.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
